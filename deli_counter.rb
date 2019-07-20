eb1803c723d6d880e55486e5c576b25cb208c5f1
Monique Vitche <mvitche7@gmail.com>
2019-07-20 19:53:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,20 +8,19 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
       people_in_line << "#{index + 1}. #{name}"
     end
-    puts "The line currently has: " + people_in_line.join(" ")
+    puts "The line is currently: " + people_in_line.join(" ")
   end
 end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0
-    puts "There's nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.shift}."
   end
-end 
+end
 
 def take_a_number(katz_deli, new_customer)
   katz_deli << new_customer
   puts "Welcome, #{new_customer}. You are number #{katz_deli.size} in line."
 end
-    
