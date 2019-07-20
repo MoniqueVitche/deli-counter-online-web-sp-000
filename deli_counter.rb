789b743892f5bb3deb77385df502afabdc4491c3
Monique Vitche <mvitche7@gmail.com>
2019-07-20 19:50:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  elsif
+    people_in_line = []
+    katz_deli.each_with_index do |name, index|
+      people_in_line << "#{index + 1}. #{name}"
+    end
+    puts "The line currently has: " + people_in_line.join(" ")
+  end
+end
